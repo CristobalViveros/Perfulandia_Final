5623932eb74c5ccfc3cbcbc66e9ddad5547978cb
Cristobal Viveros <cris.viveros@duocuc.cl>
2026-06-09 12:02:04 -0400

    Delete boletas_ms directory

--- boletas_ms/src/main/java/com/example/boletas_ms/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.boletas_ms.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- boletas_ms/src/main/java/com/example/boletas_ms/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.boletas_ms.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
