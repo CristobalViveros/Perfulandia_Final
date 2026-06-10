bbfbd1afd478a51557014f1bcace05dfff83faa7
Cristobal Viveros <cris.viveros@duocuc.cl>
2026-06-10 16:56:32 -0400

    ADD SWAGGER
    añadida configuracion SecurityConfig para swagger

--- categoria_ms/src/main/java/com/example/categoria_ms/config/SecurityConfig.java
+++ categoria_ms/src/main/java/com/example/categoria_ms/config/SecurityConfig.java
@@ -14,6 +14,12 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/swagger-ui.html",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**",
+                    "/api-docs/**"
+                ).permitAll()
                 .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults());
